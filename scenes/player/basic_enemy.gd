4d4f567e978c2d81b054ee4bca8ee35b00f0c2ed
Alexander Eklund <alexeklund460@gmail.com>
2025-01-19 18:32:22 +0100

    feat: add new behavoir still not working animation

--- scenes/player/basic_enemy.gd
+++ scenes/player/basic_enemy.gd
@@ -5,6 +5,7 @@ extends CharacterBody2D
 const jump_power = -400
 const gravity = 50 
 
+
 func _physics_process(delta: float) -> void:
 	if is_on_wall() and is_on_floor():
 		velocity.y = jump_power
@@ -16,15 +17,28 @@ func _physics_process(delta: float) -> void:
 
 # Function to move the character and handle animations
 func move(dir, speed):
+
 	velocity.x = dir * speed
 	handle_animation(dir)  # Pass the direction to handle animation
 	
 # Function to handle animation based on the character's direction
 func handle_animation(dir: int):
+	anim_tree["parameters/StateMachine/conditions/is_attacking"] = false
 	if velocity == Vector2.ZERO:
 		anim_tree["parameters/StateMachine/Run/blend_position"] = 0
-	else:	
+	else:
 		if dir > 0:
 			anim_tree["parameters/StateMachine/Run/blend_position"] = 1
 		else:
 			anim_tree["parameters/StateMachine/Run/blend_position"] = -1
+
+
+func check_for_self(node):
+	if node == self:
+		return true
+	else:
+		false
+
+# Function to play attack animation based on last direction
+func play_attack(dir: float) -> void:
+	anim_tree["parameters/StateMachine/conditions/is_attacking"] = true
--- scenes/player/dice_roller.gd
+++ scenes/player/dice_roller.gd
@@ -15,17 +15,14 @@ func roll_damage() -> Attack:
 	var damage = 0
 
 	if dice_roll == 20:
-		# Critical hit
-		print("Critical hit!")
+
 		is_critical = true
 		damage = 20  # Critical hit damage
 	elif dice_roll < 10:
-		# Flat damage for rolls below 10
-		print("Flat damage: ", dice_roll)
+
 		damage = 5  # Flat damage (you can adjust this value)
 	else:
-		# Slightly more damage for rolls above 10 but below 20
-		print("Slightly more damage: ", dice_roll)
+
 		damage = randi() % 10 + 6  # Randomized damage between 6 and 15
 
 	# Set the values in the attack object
